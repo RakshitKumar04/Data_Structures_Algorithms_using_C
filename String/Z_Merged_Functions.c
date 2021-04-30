89df27ccb354decf54d5cb3f59d2e5e925dabc1e
RakshitKumar-04 <72027411+RakshitKumar04@users.noreply.github.com>
2021-04-30 15:44:21 +0530

    Mixed function

--- String/Z_Merged_Functions.c
+++ String/Z_Merged_Functions.c
@@ -1,30 +1,77 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
+
+void appending();
+void comparing();
+void inserting();
+void reversing();
+void size();
+void substring();
+void upper_lower();
 
 int main(void)
 {
+    int a;
     do
     {
-        /* code */
-    } while (/* condition */);
-    
+        printf("\nPlease enter any one no. to proceed");
+        printf("\n1. For Appending two strings.\n2. For comparing two strings.\n3. For inserting a string at a specific position.");
+        printf("\n4. For reversing a string.\n5. To find the size of the string.\n6. For extracting a substring.\n7. For converting upper case to lower case and vice versa.\nEnter '0' to exit.\n");
+        scanf("%i", &a);
+        switch (a)
+        {
+        case 1:
+            appending();
+            break;
+        case 2:
+            comparing();
+            break;
+        case 3:
+            inserting();
+            break;
+        case 4:
+            reversing();
+            break;
+        case 5:
+            size();
+            break;
+        case 6:
+            substring();
+            break;
+        case 7:
+            upper_lower();
+            break;
+        default:
+            printf("SORRY!\nWrong Input.");
+            exit(0);
+        }
+    } while (a != 0);
+    return 0;
 }
 
-appending()
+void appending()
 {
     char des_str[50];
-    printf("Please enter a destination string : ");
-    gets(des_str);
     char sou_str[50];
+    printf("Please enter a destination string : ");
+    gets(des_str);  
     printf("Please enter the source string : ");
     gets(sou_str);
-    strcat(des_str,sou_str);
-    printf("Appending using inbuilt function : ");
+    int i, j = 0;
+    int len = strlen(des_str);
+    while (sou_str[j] != '\0')
+    {
+        des_str[len] = sou_str[j];
+        len++;
+        j++;
+    }
+    des_str[len] = '\0';
+    printf("String after appending : ");
     puts(des_str);
 }
 
-comparing()
+void comparing()
 {
     char str[50], str2[50];
     printf("Please enter 1st string : ");
@@ -33,25 +80,27 @@ comparing()
     gets(str2);
     int len = strlen(str);
     int len2 = strlen(str2);
+    int i, same = 0;
     if (len == len2)
     {
-        int res = strcmp(str,str2);
-        if (res == 0)
-        {
-            printf("Strings are equal.");
-        }
-        else
-        {
-            printf("Strings are not equal.");
-        }
+
+        printf("Strings are equal.");
     }
     else
     {
-        printf("Strings are not equal.");
+        printf("Strings are not equal.\n");
+        if (len > len2)
+        {
+            printf("First strings is greater than second string.");
+        }
+        else
+        {
+            printf("Second strings is greater than first string.");
+        }
     }
 }
 
-inserting()
+void inserting()
 {
     char sent[100];
     printf("Please enter a sentence : ");
@@ -59,6 +108,7 @@ inserting()
     char substr[50];
     printf("Enter the string to be inserted : ");
     gets(substr);
+    strcat(substr," ");
     int pos;
     printf("Enter the position at which the string has to be inserted : ");
     scanf("%i", &pos);
@@ -85,20 +135,29 @@ inserting()
     comp_str[j] = '\0';
     printf("The new string is : ");
     puts(comp_str);
-    return 0;
 }
 
-reversing()
+void reversing()
 {
-    char str[50], *str2[50];
+    char str[50]; // rev_str[50];
     printf("Please enter string : ");
     gets(str);
-    *str2 = strrev(str);
+    int i = 0;
+    int j = strlen(str) - 1;
+    while (i < j)
+    {
+        char temp;
+        temp = str[j];
+        str[j] = str[i];
+        str[i] = temp;
+        i++;
+        j--;
+    }
     printf("Reversed string : ");
-    puts(*str2);
+    puts(str);
 }
 
-size() 
+void size()
 {
     char str[50];
     printf("Please enter a string : ");
@@ -108,19 +167,18 @@ size()
     {
         len++;
     }
-    printf("Length of the string using loop : %i", len);
-    return 0;
+    printf("Length of the string : %i", len);
 }
 
-substring()
+void substring()
 {
     char str[100], substr[100];
     int i = 0, j = 0, n = 0, m;
-    printf("\n Enter the main string : ");
+    printf("Enter the main string : ");
     gets(str);
-    printf("\n Enter the position from which to start the substring: ");
+    printf("Enter the position from which to start the substring : ");
     scanf("%d", &m);
-    printf("\n Enter the length of the substring: ");
+    printf("Enter the length of the substring : ");
     scanf("%d", &n);
     i = m;
     while (str[i] != '\0' && n > 0)
@@ -128,15 +186,14 @@ substring()
         substr[j] = str[i];
         i++;
         j++;
-        n=n-1;
+        n = n - 1;
     }
     substr[j] = '\0';
     printf("\n The substring is : ");
     puts(substr);
-    return 0;
 }
 
-upper_lower()
+void upper_lower()
 {
     char str[50];
     printf("Please enter a string : ");
