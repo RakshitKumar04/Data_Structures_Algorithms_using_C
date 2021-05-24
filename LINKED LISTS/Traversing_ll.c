726778424ccde6f53759acce6a27d20b809c201c
RakshitKumar-04 <72027411+RakshitKumar04@users.noreply.github.com>
2021-05-24 12:36:36 +0530

    Commited : Traversing

---
+++ LINKED LISTS/Traversing_ll.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *start = NULL;
+struct node *create_ll(struct node *start,int n);
+struct node *display_ll(struct node *start);
+
+int main(void)
+{
+    int n;
+    printf("Please enter the length of linked list : ");
+    scanf("%i",&n);
+    start = create_ll(start,n);
+    printf("Linked List : ");
+    display_ll(start);
+    return 0;
+}
+
+int input()
+{
+    int num;
+    scanf("%i",&num);
+    return num;
+}
+
+struct node *create_ll(struct node *start,int n)
+{
+    struct node *new_node, *ptr;
+    printf("Please enter the number : ");
+    while (n != 0)
+    {
+        int val = input();
+        new_node = (struct node *)malloc(sizeof(struct node));
+        new_node->data = val;
+        if (start == NULL)
+        {
+            start = new_node;
+            new_node->next = NULL;
+        }
+        else
+        {
+            ptr = start;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
+            }
+            ptr->next = new_node;
+            new_node->next = NULL;
+        }
+        n--;
+    }
+    return start;
+}
+
+struct node *display_ll(struct node *start)
+{
+    struct node *ptr;
+    ptr = start;
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    return start;
+}
