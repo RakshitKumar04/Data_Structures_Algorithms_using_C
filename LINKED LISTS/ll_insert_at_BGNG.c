1bcf8e1483fd7e9943f0fe2f19d8d24893d65b50
RakshitKumar-04 <72027411+RakshitKumar04@users.noreply.github.com>
2021-05-24 13:00:07 +0530

    Commited : insertion at beginning

---
+++ LINKED LISTS/ll_insert_at_BGNG.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *start = NULL;
+struct node *create_ll(struct node *start, int n);
+struct node *ll_insert_at_bgng(struct node *start);
+struct node *display_ll(struct node *start);
+
+int main(void)
+{
+    int n;
+    printf("Please enter the length of linked list : ");
+    scanf("%i", &n);
+    start = create_ll(start, n);
+    printf("Linked List : ");
+    display_ll(start);
+    start = ll_insert_at_bgng(start);
+    printf("Linked List after insertion at beginning : ");
+    display_ll(start);
+    return 0;
+}
+
+int input()
+{
+    int num;
+    scanf("%i", &num);
+    return num;
+}
+
+struct node *create_ll(struct node *start, int n)
+{
+    struct node *new_node, *ptr;
+    printf("Please enter the number : ");
+    while (n != 0)
+    {
+        int val = input();
+        new_node = (struct node *)malloc(sizeof(struct node));
+        new_node->data = val;
+        if (start == NULL)
+        {
+            start = new_node;
+            new_node->next = NULL;
+        }
+        else
+        {
+            ptr = start;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
+            }
+            ptr->next = new_node;
+            new_node->next = NULL;
+        }
+        n--;
+    }
+    return start;
+}
+
+struct node *ll_insert_at_bgng(struct node *start)
+{
+    printf("\nPlease enter the value to be inserted at the begining : ");
+    int val = input();
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = val;
+    new_node->next = start;
+    start = new_node;
+    return start;
+}
+
+struct node *display_ll(struct node *start)
+{
+    struct node *ptr;
+    ptr = start;
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    return start;
+}
